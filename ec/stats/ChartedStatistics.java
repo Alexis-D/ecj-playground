1c165eabd55946ab9d9becd4ff186d5ed2a5b964
Alexis Daboville <alexis.daboville@gmail.com>
2012-11-28 15:32:13 +0100

    Handle multiple subpops

--- ec/stats/ChartedStatistics.java
+++ ec/stats/ChartedStatistics.java
@@ -7,46 +7,46 @@ import ec.util.*;
 import ec.vector.*;
 
 public class ChartedStatistics extends XYSeriesChartStatistics {
-    // public static final String P_CHARTED_STATISTICS = "charted-statistics";
-    // public static final String P_CHART_FILENAME = "chart-filename";
-    // private String chartFilename;
-    float bestFitnessSoFar = -1.f;
+    Fitness[] bestFitnessesSoFar;
 
     public void setup(EvolutionState state, final Parameter base) {
         super.setup(state, base);
 
-    //    Parameter def = defaultBase();
-    //    chartFilename = state.parameters.getStringWithDefault(base.push(P_CHART_FILENAME), def.push(P_CHART_FILENAME), "out.png");
-
         // TODO(alexis): make this parameters.
-        addSeries("Best fitness");
-        addSeries("Average fitness");
         xlabel = "Number of generations";
         ylabel = "Fitness";
         title = "Evolution of fitness over time";
     }
 
-    // public Parameter defaultBase() {
-    //     return super.defaultBase().push(P_CHARTED_STATISTICS);
-    // }
+    public void postInitializationStatistics(EvolutionState state) {
+        super.postInitializationStatistics(state);
 
-    public void postEvaluationStatistics(final EvolutionState state) {
+        bestFitnessesSoFar = new Fitness[state.population.subpops.length];
+
+        for(int i = 0; i < bestFitnessesSoFar.length; ++i) {
+            // java 1.4 no String.format
+            addSeries("Best fitness (subpop #" + Integer.toString(i) + ")");
+            addSeries("Average fitness (subpop #)" + Integer.toString(i) + ")");
+        }
+    }
+
+    public void postEvaluationStatistics(EvolutionState state) {
         super.postEvaluationStatistics(state);
 
-        float fitnessesSum = 0.f;
+        for(int i = 0; i < bestFitnessesSoFar.length; ++i) {
+            float fitnessesSum = 0.f;
 
-        // TODO(alexis): do not assume a single subpop.
-        // Java 1.4 → no for each...
-        for(int i = 0; i < state.population.subpops[0].individuals.length; ++i) {
-            Individual ind = state.population.subpops[0].individuals[i];
-            if(ind.fitness.fitness() > bestFitnessSoFar) {
-                bestFitnessSoFar = ind.fitness.fitness();
+            for(int j = 0; j < state.population.subpops[i].individuals.length; ++j) {
+                Individual ind = state.population.subpops[i].individuals[j];
+                if(bestFitnessesSoFar[i] == null || ind.fitness.betterThan(bestFitnessesSoFar[i])) {
+                    bestFitnessesSoFar[i] = ind.fitness;
+                }
+
+                fitnessesSum += ind.fitness.fitness();
             }
 
-            fitnessesSum += ind.fitness.fitness();
+            addDataPoint(0, state.generation, bestFitnessesSoFar[i].fitness());
+            addDataPoint(1, state.generation, fitnessesSum / state.population.subpops[i].individuals.length);
         }
-
-        addDataPoint(0, state.generation, bestFitnessSoFar);
-        addDataPoint(1, state.generation, fitnessesSum / state.population.subpops[0].individuals.length);
     }
 }

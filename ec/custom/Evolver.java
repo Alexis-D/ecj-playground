e8ff9e5f2a7bc719ca5653abdf3800c3250d3348
Alexis Daboville <alexis.daboville@gmail.com>
2012-11-30 11:11:43 +0100

    Use CustomEvolutionState, rather than EvolutionState to keep track of the very best individual

--- ec/custom/Evolver.java
+++ ec/custom/Evolver.java
@@ -7,7 +7,7 @@ import ec.vector.*;
 
 public class Evolver extends Evolve {
     public Individual run(String[] args) {
-        EvolutionState state;
+        EvolutionState eState;
         ParameterDatabase parameters;
 
         parameters = loadParameterDatabase(args);
@@ -15,29 +15,24 @@ public class Evolver extends Evolve {
         Individual best = null;
 
         for(int i = 0; i < numJobs; ++i) {
-            state = initialize(parameters, i);
+            eState = initialize(parameters, i);
+
+            if(!(eState instanceof CustomEvolutionState)) {
+                eState.output.fatal("state should be CustomEvolutionState if ec.custom.Evolver is used.");
+            }
+
+            CustomEvolutionState state = (CustomEvolutionState)eState;
+
             state.job = new Object[1];
             state.job[0] = new Integer(i);
             state.runtimeArguments = args;
             state.run(EvolutionState.C_STARTED_FRESH);
 
-            best = pickBest(state, best);
+            if(best == null || state.getBestSoFar().fitness.betterThan(best.fitness)) {
+                best = state.getBestSoFar();
+            }
 
             cleanup(state);
-        }
-
-        return best;
-    }
-
-    private Individual pickBest(EvolutionState state, Individual best) {
-        for(int i = 0; i < state.population.subpops.length; ++i) {
-            for(int j = 0; j < state.population.subpops[i].individuals.length; ++j) {
-                Individual ind = state.population.subpops[i].individuals[j];
-
-                if(best == null || ind.fitness.betterThan(best.fitness)) {
-                    best = ind;
-                }
-            }
         }
 
         return best;

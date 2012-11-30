7c65fa0b4682d8df2398f5430e39512930ead5db
Alexis Daboville <alexis.daboville@gmail.com>
2012-11-30 11:10:57 +0100

    This EvolutionState subclass keeps track of the best individual we've seen so far

---
+++ ec/custom/CustomEvolutionState.java
@@ -0,0 +1,32 @@
+package ec.custom;
+
+import ec.*;
+import ec.simple.*;
+import ec.util.*;
+import ec.vector.*;
+
+public class CustomEvolutionState extends SimpleEvolutionState {
+    private Individual best = null;
+
+    public int evolve() {
+        int result = super.evolve();
+        updateBest();
+        return result;
+    }
+
+    private void updateBest() {
+        for(int i = 0; i < population.subpops.length; ++i) {
+            for(int j = 0; j < population.subpops[i].individuals.length; ++j) {
+                Individual ind = population.subpops[i].individuals[j];
+
+                if(best == null || ind.fitness.betterThan(best.fitness)) {
+                    best = ind;
+                }
+            }
+        }
+    }
+
+    public Individual getBestSoFar() {
+        return best;
+    }
+}
